bb45476871763779282fd167f449b30b27a710d0
VishalYadav26 <112235127+VishalYadav26@users.noreply.github.com>
2024-07-03 02:19:42 +0530

    setup DB

--- classroom_backend/src/index.js
+++ classroom_backend/src/index.js
@@ -1,1 +1,22 @@
-import express from "express"
+import express from "express";
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './.env'
+})
+
+const app = express();
+
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
+
+
