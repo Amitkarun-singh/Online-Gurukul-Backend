5d927473aba3df3b8f2b2815baae57651a819813
Amit Singh <amitkarun4559@gmail.com>
2024-07-05 00:13:41 +0530

    project setup

--- classroom_backend/src/db/index.js
+++ classroom_backend/src/db/index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config({
     path: './.env'
 })
+
 const connectDB = async () => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${process.env.DB_NAME}`)
