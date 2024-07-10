efff4d5bca24ed6601e97fc146dfd6e2fb056917
Amit Singh <amitkarun4559@gmail.com>
2024-07-11 01:39:51 +0530

    Added app.js file

--- classroom_backend/src/index.js
+++ classroom_backend/src/index.js
@@ -1,13 +1,9 @@
-import express from "express";
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
-
+import app from "./app.js"
 dotenv.config({
     path: './.env'
 })
-
-const app = express();
-
 
 connectDB()
 .then(() => {
