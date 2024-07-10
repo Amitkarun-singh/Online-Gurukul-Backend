7ca7cd6303c0fce324f661522a87f2b872023247
Amit Singh <amitkarun4559@gmail.com>
2024-07-10 15:22:10 +0530

    Added gitignore file and setup database

--- classroom_backend/src/db/index.js
+++ classroom_backend/src/db/index.js
@@ -6,7 +6,7 @@ dotenv.config({
 
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${process.env.DB_NAME}`)
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}`,{dbname: process.env.DB_NAME})
         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
         console.log("MONGODB connection FAILED ", error);
